fb9e585548996cc0208687127da7a258a31fa887
Alexandr Sobko <alexfreetibet@gmail.com>
2021-02-16 00:38:59 +0700

    Написана функция hello_user

--- 4_while1.py
+++ 4_while1.py
@@ -14,8 +14,11 @@ def hello_user():
     """
     Замените pass на ваш код
     """
-    pass
+    while True:
+        print("Как дела")
+        if input() == "Хорошо":
+            break
 
-    
+
 if __name__ == "__main__":
     hello_user()
